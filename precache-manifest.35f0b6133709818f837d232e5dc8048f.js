c849e4785f0cfb99f73c6ee81e9accbe94c8a42b
Joel Taylor <joel@codeprivy.com>
2020-10-07 13:26:25 -0700

    Updates

---
+++ precache-manifest.35f0b6133709818f837d232e5dc8048f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e060fa4362b73bca121aa2aa0ed215",
+    "url": "/index.html"
+  },
+  {
+    "revision": "145c387e955670dbc33f",
+    "url": "/static/css/main.ff5ce706.chunk.css"
+  },
+  {
+    "revision": "46d349c1c7503d74bfd7",
+    "url": "/static/js/2.91b95ff7.chunk.js"
+  },
+  {
+    "revision": "30509d4f5a184838f617a48191a2cd37",
+    "url": "/static/js/2.91b95ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "145c387e955670dbc33f",
+    "url": "/static/js/main.d2458c3a.chunk.js"
+  },
+  {
+    "revision": "4420e4bfb045ff7f8807",
+    "url": "/static/js/runtime-main.8851b494.js"
+  }
+]);
